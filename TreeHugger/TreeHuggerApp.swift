60cf60221faa9212347d3e4958cb1e28d9c62ccb
Hristo <sandevh@gmail.com>
2022-09-10 18:31:32 -0400

    Initial Commit

---
+++ TreeHugger/TreeHuggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeHuggerApp.swift
+//  TreeHugger
+//
+//  Created by Hristo Sandev on 2022-09-10.
+//
+
+import SwiftUI
+
+@main
+struct TreeHuggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
